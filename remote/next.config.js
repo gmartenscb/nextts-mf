9a020c00763eeff7ae00eb7bfc1021342bba0683
Gijs Martens <gijs.martens@coolblue.nl>
2022-12-21 22:23:20 +0100

    Add MF to remote

--- remote/next.config.js
+++ remote/next.config.js
@@ -1,6 +1,28 @@
+const { FederatedTypesPlugin } = require('@module-federation/typescript');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      level: 'log'
+    };
+
+    config.plugins.push(
+      new FederatedTypesPlugin({
+        disableDownloadingRemoteTypes: true,
+        federationConfig: {
+          name: 'remote',
+          filename: 'remoteEntry.js',
+          exposes: {
+            './RemoteButton': './components/RemoteButton'
+          }
+        }
+      })
+    );
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
