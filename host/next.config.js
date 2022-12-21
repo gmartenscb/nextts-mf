9ec10374137cc9ec34e1cb92b763292ba0b6acfb
Gijs Martens <gijs.martens@coolblue.nl>
2022-12-21 22:32:08 +0100

    Add MF to host

--- host/next.config.js
+++ host/next.config.js
@@ -1,6 +1,28 @@
+const { FederatedTypesPlugin } = require('@module-federation/typescript');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      level: 'log'
+    };
+
+    config.plugins.push(
+      new FederatedTypesPlugin({
+        disableTypeCompilation: true,
+        federationConfig: {
+          name: 'host',
+          filename: 'remoteEntry.js',
+          remotes: {
+            remote: 'remote@http://localhost:3001/remoteEntry.js'
+          }
+        }
+      })
+    );
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
--- host/pages/index.tsx
+++ host/pages/index.tsx
@@ -1,9 +1,13 @@
+import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { Suspense } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+const RemoteButton = dynamic(() => import('remote/RemoteButton'), { suspense: true });
 
 export default function Home() {
   return (
@@ -40,23 +44,9 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+          <Suspense fallback="Loading remote button">
+            <RemoteButton />
+          </Suspense>
         </div>
 
         <div className={styles.grid}>
